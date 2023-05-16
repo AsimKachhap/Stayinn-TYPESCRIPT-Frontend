616cdaa5a22e2449bce273cbcb8ec4fcf6af35cb
unknown <asim26k@gmail.com>
2023-05-16 22:55:18 +0530

    fixed mapbox url issues

--- src/components/LeafletMap.tsx
+++ src/components/LeafletMap.tsx
@@ -1,9 +1,36 @@
 import { FunctionComponent } from "react";
+import { MapContainer, TileLayer, useMap, Marker, Popup } from "react-leaflet";
+import "leaflet/dist/leaflet.css";
 
 interface LeafletMapProps {}
 
 const LeafletMap: FunctionComponent<LeafletMapProps> = () => {
-  return <div>LeafletMap</div>;
+  const AccessToken = import.meta.env.VITE_MAPBOX_ACCESS_TOKEN;
+  const mapboxId = import.meta.env.VITE_MAPBOX_STYLE_ID;
+  const mapboxUrl = `https://api.mapbox.com/styles/v1/${mapboxId}/tiles/{z}/{x}/{y}@2x?access_token=${AccessToken}`;
+
+  return (
+    <div>
+      <MapContainer
+        center={[28.6304, 77.2177]}
+        zoom={13}
+        scrollWheelZoom={false}
+        style={{ height: 400 }}
+      >
+        <TileLayer
+          attribution='&copy; <a href="https://www.mapbox.com/about/maps/">Mapbox</a>'
+          url={mapboxUrl}
+          id={mapboxId}
+          accessToken={AccessToken}
+        />
+        <Marker position={[28.6304, 77.2177]}>
+          <Popup>
+            A pretty CSS3 popup. <br /> Easily customizable.
+          </Popup>
+        </Marker>
+      </MapContainer>
+    </div>
+  );
 };
 
 export default LeafletMap;
